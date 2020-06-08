b53b4444f3cf8ea0cd0c78ca7ab5d04293167221
viviantom0506 <41651970+viviantom0506@users.noreply.github.com>
2020-06-08 09:05:26 -0400

    Added more links for easier navigation

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,9 +9,18 @@ class Home extends Component {
           <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
           <h1>Bank of React</h1>
           <Link to="/userProfile">User Profile</Link>
+            <br></br>
+            <Link to="/UserProfile">User Profile</Link>
+            <br></br>
+            <Link to="/Debits">Debit Page</Link>
+            <br></br>
+            <Link to="/Login">Login Page</Link>
+            <br></br>
+           
           <AccountBalance accountBalance={this.props.accountBalance}/>
         </div>
     );
   }
 }
+
 export default Home;
